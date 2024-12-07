9b7c3b5d964165522ad1d053c5a68bf58354553b
Luuk Blankenstijn <luukblankenstijn@gmail.com>
2024-12-07 19:08:56 +0100

    chore: add timing to template.go

--- solutions/template.go
+++ solutions/template.go
@@ -1,13 +1,23 @@
 package solutions
 
-func Dayn() {
+import (
+	"fmt"
+	"time"
+)
 
+func Dayn() (int, int) {
+	start := time.Now()
+	parseInputDayn()
+	part1, part2 := solutionDayn()
+	elapsed := time.Since(start)
+	fmt.Println("Solution day n time:", elapsed)
+	return part1, part2
 }
 
 func parseInputDayn() {
 
 }
 
-func solutionDayn() {
-
+func solutionDayn() (int, int) {
+	return 0, 0
 }
